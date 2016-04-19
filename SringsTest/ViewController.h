48ad00d6e81499ba3bfadb2867f5773fa34cf4fa
vitilius <vitilius1@gmail.com>
2016-04-19 16:18:39 +0300

    HW

---
+++ SringsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SringsTest
+//
+//  Created by Viktor Lukashevich on 18.04.16.
+//  Copyright © 2016 Viktar Lukashevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
